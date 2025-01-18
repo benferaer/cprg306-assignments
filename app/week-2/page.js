a0edf1a4212900dbd0d8a7418aa2ab7100d366fa
benferaer-saitacc <jeanbennett.feraer@edu.sait.ca>
2025-01-18 16:46:13 -0700

    Week 2 code complete

---
+++ app/week-2/page.js
@@ -0,0 +1,10 @@
+import StudentInfo from "./student-info";
+
+export default function Page() {
+    return (
+      <main>
+        <h1>Student Info</h1>
+        <StudentInfo />
+      </main>
+    );
+  }
---
+++ app/week-2/student-info.js
@@ -0,0 +1,9 @@
+export default function StudentInfo() {
+    return (
+    <div>
+        <h1>Name: Ben Feraer</h1>
+        <p><a href="https://github.com/benferaer/cprg306-assignments">This is a link to my github repository</a></p>
+        <p><a href="/">Back to root page</a></p>
+    </div>
+    );
+}
