96f4b23c616052d60f58d121e7983b06b15f84fc
benferaer <benferaer@gmail.com>
2025-02-01 11:32:35 -0700

    push for week 3 assignment

--- app/page.js
+++ app/page.js
@@ -3,6 +3,7 @@ export default function Page() {
       <main>
         <h1>CPRG 306: Web Development 2 - Assignments</h1>
         <p><a href = "./week-2">Week 2 page here</a></p>
+        <p><a href = "./week-3">Week 3 page here</a></p>
       </main>
     );
   }
