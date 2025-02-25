e794078e80aa862429fd72a8115bd010fe640808
benferaer <benferaer@gmail.com>
2025-02-24 17:28:12 -0700

    Week 5 done

--- app/page.js
+++ app/page.js
@@ -5,6 +5,7 @@ export default function Page() {
         <p><a href = "./week-2">Week 2 page here</a></p>
         <p><a href = "./week-3">Week 3 page here</a></p>
         <p><a href = "./week-4">Week 4 page here</a></p>
+        <p><a href = "./week-5">Week 5 page here</a></p>
       </main>
     );
   }
